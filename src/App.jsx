39432cc1c91716dc161af97d9ebb82284783c552
mauriciosilvera <mauricioagustin_@outlook.com>
2023-02-01 17:29:14 -0300

    Hero, Navbar, Stats and GetStarted components done

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
-import styles from './style'
-import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components'
+import React from 'react';
+import styles from './style';
+import {
+  Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer,
+} from './components';
 
-const App = () => {
+function App() {
   return (
-    <div className='bg-primary w-full overflow-hidden'>
+    <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           <Navbar />
         </div>
       </div>
 
-      <div className={`bg-primary ${styles.flexStart}`}> 
+      <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          <Hero/>
+          <Hero />
         </div>
       </div>
 
-      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}> 
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
           <Stats />
           <Business />
@@ -30,7 +32,7 @@ const App = () => {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
