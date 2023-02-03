df6df6bec2166b40a041678cf44e130da3dd3498
mauriciosilvera <mauricioagustin_@outlook.com>
2023-02-03 02:20:14 -0300

    Billing component finished.

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import { apple, bill, google } from '../assets';
+import styles, { layout } from '../style';
 
 function Billing() {
   return (
-    <div>Billing</div>
+    <section id="product" className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img src={bill} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+        <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
+      </div>
+
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Easily control your
+          {' '}
+          <br className="sm:block hidden" />
+          {' '}
+          billing & invoicing.
+        </h2>
+
+        <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+          <img src={apple} alt="Apple Store" className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer" />
+          <img src={google} alt="Google Play" className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer" />
+        </div>
+      </div>
+    </section>
   );
 }
 
